b4c66275e0732f3963aa52a35b6e0512325ac289
Victor Jozwicki <tigre.demon@hotmail.fr>
2015-10-07 17:42:04 +0200

    Ah bah ...

--- controllers/signin.php
+++ controllers/signin.php
@@ -1,5 +1,42 @@
 <?php
 
+if( $_POST ) {
+        if( isset($_POST['login']) && strlen($_POST['login']) >= 5  ) {
+        $existingLogin = UserDAO::getByLogin($_POST['login']);
+        var_dump($existingLogin);
+        $login = $_POST['login'];
+        $valid['login'] = true;
+    } else {
+        $valid['login'] = false;
+    }
+
+    if( isset($_POST['password']) && strlen($_POST['password']) >= 5  ) {
+        $password = $_POST['password'];
+        $valid['password'] = true;
+    } else {
+        $valid['password'] = false;
+    }
+
+    if( $_POST['email'] != NULL) {
+        $mail = $_POST['email'];
+        $valid['email'] = true;
+    } else {
+        $valid['email'] = false;
+    }
+
+    if( $valid['login'] && $valid['password'] ) {
+        require_once MODELS_INC.'UserDAO.class.php';
+        include('passwordHash.inc.php');
+        $password = create_hash($password);
+        $newUser = new User($login, $password, $_POST['firstName'], $_POST['lastName'], $_POST['sex'], $_POST['email'], $_POST['birthDate'], $_POST['address'], $_POST['postalCode'], $_POST['city'], $_POST['phoneNumber']);
+        //var_dump($newUser);
+        //UserDAO::create($newUser);
+        //header('Location: connection');
+    }
+
+}
+
+
 include(VIEWS_INC.'signin.php');
 
 ?>
--- views/signin.php
+++ views/signin.php
@@ -3,24 +3,50 @@
 <div>
     <section>
         <form method="post">
-            <label for="login">Nom d'utilisateur</label>
-            <input type="text" name="login" id="login" value='<?php if ($_POST) {
-                echo $_POST['login'];
-            } ?>' />
+            <section>
+                <label for="login">Nom d'utilisateur</label>
+                <span>5 caractères minimum</span>
+                <input type="text" name="login" id="login" placeholder="nom d'utilisateur" value='<?php if ($_POST) {
+                    echo $_POST['login'];
+                } ?>' />
 
-            <label for="password">Mot de passe</label>
-            <input type="password" name="password" id="password" />
+                <label for="password">Mot de passe</label>
+                <span>5 caractères minimum</span>
+                <input type="password" name="password" id="password" placeholder="mot-de-passe"/>
 
-            <label name="mail">E-mail</label>
-            <input type="email" name="mail" id="mail" placeholder="exemple@exemple.com" value='<?php if ($_POST) {
-                echo $_POST['mail'];
-            } ?>' />
+                <label name="email">E-mail</label>
+                <input type="email" name="email" id="mail" placeholder="exemple@exemple.com" value='<?php if ($_POST) {
+                    echo $_POST['email'];
+                } ?>' />
+            </section>
+            <section>
+                <label for="lastName">Nom</label>
+                <input type="text" name="lastName" value="<?php if( $_POST ) { echo $_POST['lastName']; }?>"/>
 
-            <label name="mailConfirmation">Confirmation adresse Email</label>
-            <input type="email" name="mailConfirmation" id="mailConfirmation" placeholder="exemple@exemple.com" value='<?php if ($_POST) {
-                echo $_POST['mailConfirmation'];
-            } ?>' />
+                <label for="firstName">Prénom</label>
+                <input type="text" name="firstName" value="<?php if( $_POST ) { echo $_POST['firstName']; }?>"/>
 
+                <label for="sex">Sexe</label>
+                <select name="sex">
+                    <option value="m">Homme</option>
+                    <option value="f">Femme</option>
+                </select>
+
+                <label for="birthDate">Date de naissance</label>
+                <input type="date" name="birthDate" value="<?php if( $_POST ) { echo $_POST['birthDate']; }?>"/>
+
+                <label for="address">Adresse</label>
+                <input type="text" name="address" value="<?php if( $_POST ) { echo $_POST['address']; }?>"/>
+
+                <label for="postalCode">Code postal</label>
+                <input type="text" name="postalCode" value="<?php if( $_POST ) { echo $_POST['postalCode']; }?>"/>
+
+                <label for="city">Ville</label>
+                <input type="text" name="city" value="<?php if( $_POST ) { echo $_POST['city']; }?>"/>
+
+                <label for="phoneNumber">Téléphone</label>
+                <input type="text" name="phoneNumber" value="<?php if( $_POST ) { echo $_POST['phoneNumber']; }?>"/>
+            </section>
             <input type="submit" name="enregistrer" value="S'enregistrer"/>
         </form>
     </section>

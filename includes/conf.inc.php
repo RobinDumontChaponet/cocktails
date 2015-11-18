75842e7d0b22d3885858ff4a5404fcbf35eae7d8
Robin Dumont-Chaponet <robin@dumontchapo.net>
2015-11-18 01:48:48 +0100

    test ?

--- includes/conf.inc.php
+++ includes/conf.inc.php
@@ -6,7 +6,7 @@ namespace Transitive;
  * Architecture-related
  */
 define('ROOT_PATH', dirname(dirname(__FILE__)));
-define('SELF', (dirname($_SERVER['PHP_SELF']) == '/' ? '' : dirname($_SERVER['PHP_SELF'])));
+define('SELF', dirname($_SERVER['PHP_SELF']));
 define('CONTROLLERS_INC', ROOT_PATH.'/controllers/');
 define('MODELS_INC', ROOT_PATH.'/models/');
 define('VIEWS_INC', ROOT_PATH.'/views/');
